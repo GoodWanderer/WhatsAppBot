f80029cf0cf7106f92000e216dcb2e836901b054
Макар <goodwanderer@outlook.com>
2021-04-16 22:29:26 +0500

    1

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    // URL адрес для обращений к API
+    apiUrl: "https://api.chat-api.com/instance256149/",
+    // Токен для работы с API из личного кабинета
+    token: "mdo7z3a6zmiytw2g"
+}
